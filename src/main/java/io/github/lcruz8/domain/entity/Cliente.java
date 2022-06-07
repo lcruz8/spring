f7890a887b7876b44dd3f90bb0aaeb169ba537b5
L.M <l_mcruz98@live.com>
2022-06-06 22:35:07 -0300

    Mapeando a entity cliente

--- src/main/java/io/github/lcruz8/domain/entity/Cliente.java
+++ src/main/java/io/github/lcruz8/domain/entity/Cliente.java
@@ -1,7 +1,17 @@
 package io.github.lcruz8.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+//@Table - Mais utilizado quando a classe não tem o mesmo nome da tabela, ou quando se precisa de
+// mais informações sobre schema, e etc.
+@Table(name = "Cliente", schema = "")
+
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // Estratégia de geração do valor (auto-incremento)
     private Integer id;
+    @Column(name = "id", length = 100, updatable = false) //Não obrigatório para colunas com o mesmo nome no bd
     private String nome;
     private String telefone;
 
